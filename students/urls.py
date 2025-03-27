d4d824a2337bfeda3249e09e2f646dbeebfd7d64
Edmond Zuberi <edmond.norman.zuberi@gmail.com>
2025-03-28 00:55:04 +0300

    Initial commit

---
+++ students/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "students"
+urlpatterns = [
+    # path("", views.index, name="index"),
+    # path("add/", views.add, name="add"),
+    # path("edit/<int:id>/", views.edit, name="edit"),
+    # path("delete/<int:id>/", views.delete, name="delete"),
+    # path("search/", views.search, name="search"),
+]
